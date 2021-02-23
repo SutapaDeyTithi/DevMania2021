8f8467660b2251ebc123f80c3d215695aed1f791
SutapaDeyTithi <64492414+SutapaDeyTithi@users.noreply.github.com>
2021-02-23 18:20:53 +0600

    updating the SCRATCH files to app (mentalhealth) level files

---
+++ Dev Mania/Practice Code/StudyDeskBackEnd/server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var knex = require('knex')
+var bcrypt = require('bcrypt-nodejs');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '521991889',
+    database : 'Mental Health'
+  }
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+/********* in front end, there is no prompt for duplicate email but no registration/insertion happens in database **********/
+/********* in front end, there is no conditioning yet for this issue **********/
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => {
+ const { id } = req.params;
+ let found = false;
+ db.select('*').from('users').where({ id })
+ .then(user => {
+  if(user.length)
+    res.json(user[0]);
+  else
+    res.status(404).json('Not found');
+ })
+ // if(!found){
+ //  res.status(404).json('not found');
+ // }
+})
+
+app.put('/post', (req, res) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('posts', 1)
+  .returning('posts')
+  .then(posts => {
+    res.json(posts[0]);
+  })
+  .catch(err => res.status(404).json('not found user'));
+})
+
+// process.env.PORT
+app.listen(3001, () => 
+  console.log('Example app listening on port 3001',)
+  )
--- Dev Mania/Practice Code/readMe.txt
+++ Dev Mania/Practice Code/readMe.txt
@@ -1,7 +1,4 @@
-1. front end:
-	a. create-react-app appname
-	b. npm start
-2. back end:
+1. back end:
 	a. npm init -y
 	b. npm install nodemon --save-dev
 	c. go to package.json
@@ -12,4 +9,30 @@
 	d. create index.js file in the same folder where package.json resides
 	e. in cmd prompt, run:
 		> npm start
-	f. now watch videos from folder 24, video no 08.
+	f. npm install express
+	   npm install body-parser
+	   npm install cors
+	   npm install knex
+	   npm install bcrypt-nodejs
+	   npm install pg --save
+	g. Copy package.json, server.js, controllers from mentalhealthAPI to this new backend folder
+		- update the last part of server.js as below:
+			// process.env.PORT
+			app.listen(3001, () => 
+  			console.log('Example app listening on port 3001',)
+  			)
+	h. in cmd prompt, run:
+		> npm start
+	i. 
+
+	z. now watch videos from folder 24, video no 08.
+
+2. front end:
+	a. create-react-app appname
+	b. copy src folder from mentalhealth frontend folder to this fronend folder	
+	c. npm install react-particles-js
+	d. npm install @types/react
+	e. npm run build
+	f. npm start
+
+	z. watch videos from folder 19 (zero to mastery).
